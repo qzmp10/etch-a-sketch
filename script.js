3bed842835b981eac7156c1bd67812511d3fd30f
qzmp10 <loganrobitaille@outlook.com>
2022-04-25 18:58:04 -0400

    makeNewGrid function creates a new grid but the color change does not work on the new grid and pressing the reset button again does not remove the newly created grid but adds another grid on top of it. will ask TOP discord

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+//creating the grid 
 let grid = document.createElement('div');
 let page = document.querySelector('.page');
 page.appendChild(grid);
 grid.classList.add('grid');
 
-//create a 16x16 grid
+//creating the button that resets the grid color
+let resetBtn = document.createElement('button');
+resetBtn.classList.add('reset');
+resetBtn.textContent = "Reset Grid Colors";
+page.appendChild(resetBtn);
+
+
+//function to create a 16x16 grid
 function makeGrid() {
     for (i=0; i<256; i++) {
         let item = document.createElement('div');
         grid.appendChild(item);
     }
 }
-
 makeGrid();
 
 // let each grid piece change to a specific color when a mouseover action is performed
@@ -29,12 +36,28 @@ let resetColor = () => {
     })
 }
 
-let resetBtn = document.createElement('button');
-resetBtn.classList.add('reset');
-resetBtn.textContent = "Reset Grid Colors";
-page.appendChild(resetBtn);
 
-resetBtn.addEventListener('click', () => resetColor());
+// divs = grid.querySelectorAll('div')
+// so for each div in grid, remove itself before creating the new grid which has the users input as a base number for the grid
+function makeNewGrid() {
+        divs.forEach(div => {
+            div.remove();
+        })
+    let newGrid = prompt('New grid size? Input a number. Example: 20 will display a 20x20 grid. Cannot be over 20.');
+    for (i=0; i<(newGrid * newGrid); i++) {
+        let item = document.createElement('div');
+        grid.appendChild(item);
+    } 
+}
+
+//what happens when u click the reset colors button
+resetBtn.addEventListener('click', () => {
+resetColor()
+makeNewGrid()
+});
+
+
+
 
 
 
