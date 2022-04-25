dbaf871f137772b3ff4c263f832785c35482a50e
qzmp10 <loganrobitaille@outlook.com>
2022-04-25 17:57:11 -0400

    Finished up the changes color on mouseover function and the resetScores function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ page.appendChild(grid);
 
 grid.classList.add('grid');
 
+//create a 16x16 grid
 function makeGrid() {
     for (i=0; i<256; i++) {
         let item = document.createElement('div');
@@ -14,6 +15,25 @@ function makeGrid() {
 }
 
 makeGrid();
+
+// let each grid piece change to a specific color when a mouseover action is performed
+let divs = grid.querySelectorAll('div');
+
+divs.forEach((div => {
+    div.addEventListener('mouseover', function() {
+    div.setAttribute('style', 'background-color: #FEAE51FF;');
+})
+}));
+
+// function to reset the grid colors to the default
+let resetColor = () => {
+    divs.forEach(div => {
+        div.setAttribute('style', 'background-color: white;');
+    })
+}
+
+
+
 
 
 
