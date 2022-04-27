125b1759d846409b37706a568ddf6cd50ee2ef20
qzmp10 <loganrobitaille@outlook.com>
2022-04-27 16:50:09 -0400

    Pretty much finished etch-a-sketch but gonna try to make a rainbow color mouseover event

--- script.js
+++ script.js
@@ -6,20 +6,15 @@ let page = document.querySelector('.page');
 page.appendChild(grid);
 grid.classList.add('grid');
 
-//creating the button that resets the grid color
-let resetBtn = document.createElement('button');
-resetBtn.classList.add('reset');
-resetBtn.textContent = "Reset grid colors";
-page.appendChild(resetBtn)
-
 //create input buttons that dictates the size of the grid
 let btnContainer = document.createElement('div');
 btnContainer.classList.add('btncontainer');
 page.appendChild(btnContainer);
 
 let gridSize = document.createElement('div');
-gridSize.classList.add('btn');
-gridSize.textContent = 'Set grid size';
+gridSize.classList.add('sizeGrid');
+gridSize.id = 'id'
+gridSize.textContent = 'Set grid size :';
 btnContainer.appendChild(gridSize);
 
 let gridBtn16 = document.createElement('button');
@@ -39,6 +34,7 @@ btnContainer.appendChild(gridBtn64);
 
 //when user clicks button, set grid size
 //if grid already has elements, remove them
+// when user mouseover grid elements -> turn them to specified color
 
 //16x16
 gridBtn16.addEventListener('click', () => {
@@ -53,6 +49,12 @@ gridBtn16.addEventListener('click', () => {
     grid.style.cssText = 'gap: 0.3em;';
     grid.style.cssText = 'grid-template-rows: repeat(16, 1fr);';
     grid.style.cssText = 'grid-template-columns: repeat(16, 1fr);';
+    gridItems = grid.querySelectorAll('div');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('style', 'background-color: blueviolet;');
+        });
+    });
 });
 
 //32x32
@@ -68,6 +70,12 @@ gridBtn32.addEventListener('click', () => {
     grid.style.cssText = 'gap: 0.15em;';
     grid.style.cssText = 'grid-template-rows: repeat(32, 1fr);';
     grid.style.cssText = 'grid-template-columns: repeat(32, 1fr);';
+    gridItems = grid.querySelectorAll('div');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('style', 'background-color: blueviolet;');
+        });
+    });
 });
 
 //64x64
@@ -83,7 +91,59 @@ gridBtn64.addEventListener('click', () => {
     grid.style.cssText = 'gap: 0.075em;';
     grid.style.cssText = 'grid-template-rows: repeat(64, 1fr);';
     grid.style.cssText = 'grid-template-columns: repeat(64, 1fr);';
+    gridItems = grid.querySelectorAll('div');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.setAttribute('style', 'background-color: blueviolet;');
+        });
+    });
 });
+
+//creating the button that resets the grid color
+let resetBtn = document.createElement('button');
+resetBtn.classList.add('reset');
+resetBtn.id = 'id';
+resetBtn.textContent = "Reset grid colors";
+page.appendChild(resetBtn)
+
+//reset the colors when user clicks on reset color
+gridItems = grid.querySelectorAll('div');
+resetBtn.addEventListener('click', () =>  {
+    gridItems.forEach(gridItem => {
+        gridItem.style = 'background-color: white;'
+    });
+});
+
+// add mouseover effects for the grid buttons just for the style
+gridBtn16.addEventListener('mouseover', () => {
+    gridBtn16.classList.add('hovered');
+});
+gridBtn16.addEventListener('mouseout', () => {
+    gridBtn16.classList.remove('hovered');
+});
+
+gridBtn32.addEventListener('mouseover', () => {
+    gridBtn32.classList.add('hovered');
+});
+gridBtn32.addEventListener('mouseout', () => {
+    gridBtn32.classList.remove('hovered');
+});
+
+gridBtn64.addEventListener('mouseover', () => {
+    gridBtn64.classList.add('hovered');
+});
+gridBtn64.addEventListener('mouseout', () => {
+    gridBtn64.classList.remove('hovered');
+});
+
+resetBtn.addEventListener('mouseover', () => {
+    resetBtn.classList.add('hovered');
+});
+resetBtn.addEventListener('mouseout', () => {
+    resetBtn.classList.remove('hovered');
+});
+
+
 
 
 
